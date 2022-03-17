8bf691d02c4b10c75660b7a1153a0a32dbb21a21
Caio Da Silva <caiodasilvatx@gmail.com>
2022-03-17 23:41:39 +0100

    function to check the game winner

--- src/main/java/com/factoriaf5/rps/application/Game.java
+++ src/main/java/com/factoriaf5/rps/application/Game.java
@@ -1,5 +1,40 @@
 package com.factoriaf5.rps.application;
 
 public class Game {
-    
+
+    public String playGame(String playerOne, String playerTwo) {
+
+        String winner = this.checkWinner(playerOne, playerTwo);
+        return winner;
+
+    }
+
+    public String checkWinner(String playerOne, String playerTwo) {
+
+        if (playerOne == "Rock" && playerTwo == "Scissors") {
+            return "Rock win";
+        }
+
+        if (playerOne == "Scissors" && playerTwo == "Rock") {
+            return "Rock win";
+        }
+
+        if (playerOne == "Rock" && playerTwo == "Paper") {
+            return "Paper win";
+        }
+
+        if (playerOne == "Paper" && playerTwo == "Rock") {
+            return "Paper win";
+        }
+
+        if (playerOne == "Scissors" && playerTwo == "Paper") {
+            return "Scissors win";
+        }
+
+        if (playerOne == "Paper" && playerTwo == "Scissors") {
+            return "Scissors win";
+        }
+
+        return "draw";
+    }
 }
