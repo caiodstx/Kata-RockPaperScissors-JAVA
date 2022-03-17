66f2447ca80c34867da568d8c9efe30d9bcd8220
Caio Da Silva <caiodasilvatx@gmail.com>
2022-03-17 23:46:09 +0100

    base app file

---
+++ src/main/java/com/factoriaf5/rps/domain/infrastructure/App.java
@@ -0,0 +1,9 @@
+package com.factoriaf5.rps.domain.infrastructure;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
